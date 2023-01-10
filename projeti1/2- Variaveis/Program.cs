86cbe02984adb4c8c8e4693530e0db9f56ccbe50
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-10 14:13:39 -0300

    adicionando projetos e variaveis

---
+++ projeti1/2- Variaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        int idade;
+        
+        idade = 25;
+        Console.WriteLine(idade);
+
+        idade = 31 - 1;
+        Console.WriteLine("Minha idade passou a ser " + idade);
+        
+        idade = 21 * 1 - 1;
+        Console.WriteLine("E então passou a ser " + idade);
+
+        idade = (5+5) * 2 + 1 - 1;
+        Console.WriteLine("Depois foi " + idade);
+
+        idade = 5 + (5 * 2);
+        Console.WriteLine("E então " + idade + " anos de Idade");
+
+        Console.ReadLine();
+    }
+}
--- projeti1/Ola Mundo/Program.cs
+++ projeti1/Ola Mundo/Program.cs
@@ -3,7 +3,9 @@ class Programa
 {
     static void Main(String[] args)
     {
+        //Console.BackgroundColor = ConsoleColor.Red; //cor de fundo das letras
         Console.WriteLine("Cachorro velho não só aprende truque novo, como fica bom neles.");
+        Console.WriteLine("Fogo Neles!");
         Console.ReadLine();
     }
 }
