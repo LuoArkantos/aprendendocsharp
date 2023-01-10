ea4bd53f9270f2641a83a057ebccbf5142f9a887
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-10 09:56:57 -0300

    adicionando arquivos C# e printando algo em console

---
+++ projeti1/Ola Mundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Cachorro velho não só aprende truque novo, como fica bom neles.");
+        Console.ReadLine();
+    }
+}
+    
