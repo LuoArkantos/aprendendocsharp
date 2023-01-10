70434ece887cb26b7639fa283d87a3f546b057aa
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-10 20:30:59 -0300

    adicionando conversao e outros tipos de var

---
+++ projeti1/3- Conversao e Outros Tipos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("4 - Conversão e Outros Tipos");
+
+        int valorInteiro = 25;
+        Console.WriteLine(valorInteiro);
+
+        double deInteiroParaDouble = (double)valorInteiro; //Recebe o valor inteiro e transforma em Double para ser fracionado
+        Console.WriteLine("De Inteiro para Double - " + deInteiroParaDouble / 2);
+
+        long numeroGrande = 2000000000000000000;
+        Console.WriteLine("Var tipo Long - " + numeroGrande);
+
+        short numeroPequeno = 15000; // Ideal para armazenar algo como Quantidade de convidados, numero de modelos ou algo de quantidade não tão grande.
+        Console.WriteLine("Var tipo Short - " + numeroPequeno);
+
+        float numeroFloat = 1.62f; //como Double, porém mais preciso e deve ter f afrente do numero
+        Console.WriteLine("Var tipo Float - " + numeroFloat);
+
+
+
+        Console.ReadLine();
+
+    }
+}
---
+++ projeti1/3- Variaveis Double (tipo float do py)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("3 - Variáveis tipo Double");
+
+        double salarioBase;
+        salarioBase = 1235.23;
+        Console.WriteLine("Salário Base: " + salarioBase);
+
+        double beneficios;
+        beneficios = 350;
+        Console.WriteLine("Valor dos Beneficios: " + beneficios);
+
+        double descontos;
+        descontos = 120.50;
+        Console.WriteLine("Total de Descontos: " + descontos);
+
+        double salarioTotal;
+        salarioTotal = salarioBase + beneficios - descontos;
+        Console.WriteLine("O Salário Base + os Benefícios - os Descontos é igual a: " + salarioTotal);
+
+        Console.ReadLine();
+    }
+}
