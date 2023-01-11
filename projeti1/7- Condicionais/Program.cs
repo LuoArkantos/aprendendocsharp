580cd91ba38da43d6a590754f25811cba9545eb8
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-11 11:14:30 -0300

    terminando basico csharp

---
+++ projeti1/7- Condicionais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("7 - Condicionais");
+
+        int idade = 18;
+        int acompanhantes = 1;
+
+        bool grupo = acompanhantes > 0; //armazena dados Booleanos (True or False)
+
+        //And == && , Or == || , Not == ! 
+        
+        if (idade >= 18 && grupo)
+        {
+            Console.WriteLine("Acesso Autorizado. Bem vindo!");
+        }
+
+        else
+        {
+            Console.WriteLine("Acesso Negado!");
+        }
+
+
+
+
+        Console.ReadLine();
+    }
+}
---
+++ projeti1/8- Escopo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("7 - Condicionais");
+
+        int idade = 18;
+        int acompanhantes = 0;
+
+        bool grupo = acompanhantes > 0; //armazena dados Booleanos (True or False)
+
+        //And == && , Or == || , Not == ! 
+
+        /// ESCOPO:
+        /// 
+        /// Se uma variável for criada dentro de um if, ou qualquer outra 
+        /// condicional ou estrutura, ela não tem efeito global, impossivel
+        /// ser usada em outro lugar fora de onde ela foi atribuida
+        ///
+        /// Retorna Valores Locais, não globais
+        /// 
+
+        bool acompanhado = acompanhantes > 0;
+        string textoSobreAcompanhante;
+
+
+        if (acompanhado)
+        {
+            textoSobreAcompanhante = "O cliente está acompanhado";
+            Console.WriteLine(textoSobreAcompanhante);
+        }
+        else
+        {
+            textoSobreAcompanhante = "O cliente está sozinho";
+            Console.WriteLine(textoSobreAcompanhante);
+        }
+
+        if (idade >= 18 && grupo)
+        {
+            Console.WriteLine("Acesso Autorizado. Bem vindo!");
+        }
+
+        else
+        {
+            Console.WriteLine("Acesso Negado!");
+        }
+
+        Console.ReadLine();
+    }
+}
