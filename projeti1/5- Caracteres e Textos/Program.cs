67e4be0531d3f515ec1ac38405eea0006cc98644
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-10 21:16:25 -0300

    caracteres e textos

---
+++ projeti1/5- Caracteres e Textos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("5 - Caracteres e Textos");
+
+        char caractere = 'a'; //Não pode ser vazio
+        Console.WriteLine(caractere);
+
+        string frase = "Quem com ferro fere, com ferro será ferido";
+        Console.WriteLine(frase);
+
+        string textoComLista = @"Lista de compras:
+- Arroz 
+- Feijão 
+- Carne 
+- Danoninho"; //Usando o @ no inicio da string, da pra pular linha
+
+        Console.WriteLine(textoComLista);
+        Console.ReadLine();
+    }
+}
