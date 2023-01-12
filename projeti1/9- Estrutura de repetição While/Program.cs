8d362b052bd8d7497a5fb0ae8960ef47e91642ba
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-12 19:20:56 -0300

    laço while

---
+++ projeti1/9- Estrutura de repetição While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Estrutura de repetição While");
+
+        double valorAplicado = 1000;
+        double porcentagemMensal = 0.005;/* 0.5% q é igual a 0.005*/;
+        int meses = 1;
+
+        while(meses <= 12)
+        {
+            double juroRendido = valorAplicado * porcentagemMensal;
+            valorAplicado = valorAplicado + juroRendido;
+
+            Console.WriteLine("Valor atualizado do mês " + meses + " é de " + valorAplicado);
+            Console.WriteLine("Houve um lucro de " + juroRendido);
+
+            meses++;
+
+        }
+
+
+        Console.WriteLine("FIM DO PROGRAMA!");
+        Console.ReadLine();
+    }
+}
