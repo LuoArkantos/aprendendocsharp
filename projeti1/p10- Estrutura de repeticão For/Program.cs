140d0357b4160ffac7b62829a036a91fb287071f
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-01-13 18:09:28 -0300

    Estrutura for e encadeamento de for

---
+++ projeti1/p10- Estrutura de repeticão For/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        /*Console.WriteLine("Estrutura de repetição While");
+
+        double valorAplicado = 1000;
+        double porcentagemMensal = 0.005;// 0.5% q é igual a 0.005;
+
+       while (meses <= 12)
+        {
+            double juroRendido = valorAplicado * porcentagemMensal;
+            valorAplicado = valorAplicado + juroRendido;
+
+            Console.WriteLine("Valor atualizado do mês " + meses + " é de " + valorAplicado);
+            Console.WriteLine("Houve um lucro de " + juroRendido);
+
+            meses++;
+
+        }*/
+        double valorAplicado = 1000;
+        double porcentagemMensal = 1.005;// 0.5% q é igual a 0.005, o 1 significa o 1ma vez o valorAplicado;
+
+        for (int meses = 1; meses <= 12; meses++)
+        {
+            valorAplicado *= porcentagemMensal;
+            Console.WriteLine("Valor atualizado do mês " + meses + " é de " + valorAplicado);
+
+        }
+
+
+        Console.WriteLine("FIM DO PROGRAMA!");
+        Console.ReadLine();
+    }
+}
---
+++ projeti1/p11- Rendimento Poupança com incremento anual (For encadeado)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO.Pipes;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        /* Encadeamento do FOR é quando usamos o FOR dentro de um laço FOR existente
+         Por Exemplo:
+        
+         O valor do rendimento cresce 1 por cento por ano, assim em cada ano, o 
+        investimento é multiplicado mensalmente pela porcentagem atualizada anual
+        dos juros.
+
+        Ex: No ano 1, o juro é de 0.5%, no ano 2 o juro é de 0.6% e aí por diante
+        No ano 1, todo mes é adicionado 0.5% ao investimento
+        No ano 2, todo mes é adicionado 0.6% ao investimento...
+        Crescente até que se atinja o teto de anos (5 anos no caso)
+
+         */
+
+        Console.WriteLine("RENDIMENTO DE POUPANÇA COM ACRESCIMO ANUAL DE JUROS");
+
+        double investimento = 1000;
+        double rendimento = 1.005;
+
+        for (int ano = 1; ano <= 5; ano++)
+        {
+            rendimento += 0.001;
+            Console.WriteLine(@"
+
+Porcentagem do ano " + ano +" foi igual a "+rendimento+@"
+
+");
+
+            for(int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= rendimento;
+                Console.WriteLine("Resultado do investimento do mes " + mes + " do ano " + ano +':');
+                Console.WriteLine(investimento + @"
+");
+            }
+            Console.WriteLine("Neste ano você acumulou o valor de R$: " + investimento);
+        }
+
+        Console.WriteLine("FIM DO PROGRAMA");
+        Console.ReadLine();
+    }
+}
